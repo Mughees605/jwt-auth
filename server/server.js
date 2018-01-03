05c3e8bd3f63d8947ef607cee8dd743c019d9be9
mughees605 <mughees605@gmail.com>
2018-01-04 03:29:19 +0500

    user login

--- server/server.js
+++ server/server.js
@@ -116,6 +116,16 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 })
 
+app.post('/users/login' , (req,res)=>{
+  var body = _.pick(req.body, ['email'], ['password']);
+
+  User.findByCredentials(body.email, body.password).then((user)=>{
+    res.send(user);
+  }).catch((err)=>{
+   res.status(400).send();
+  })
+})
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
