60be3f929a50012c1b949d5d43a5cf3aeefbcbaa
mughees605 <mughees605@gmail.com>
2018-01-04 14:31:32 +0500

    delted files

--- server/config/config.js
+++ server/config/config.js
@@ -2,8 +2,11 @@ var env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+  process.env.MONGODB_URI = 'mongodb://mohsin123:mohsin123@ds239387.mlab.com:39387/mohsin-test';
 } else if (env === 'test') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 }
+
+// mongodb://mohsin123:mohsin123@ds239387.mlab.com:39387/mohsin-test
+// mongodb://localhost:27017/TodoApp
